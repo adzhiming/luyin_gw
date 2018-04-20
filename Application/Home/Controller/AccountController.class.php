157cbb7b327110132be683f5aba01ffd5c4755b2
adzhiming <272109570@qq.com>
2018-04-20 09:46:48 +0800

    电话簿下载

--- Application/Home/Controller/AccountController.class.php
+++ Application/Home/Controller/AccountController.class.php
@@ -385,7 +385,7 @@ class AccountController extends AuthController {
     }
     
     //导出excel
-    public function downloadExcel(){
+    public function downloadPhoneExcel(){
         Vendor("excel.PHPExcel");
         Vendor("excel.PHPExcel.Reader.Excel2007.php");
         Vendor("excel.PHPExcel.Reader.Excel5");
@@ -393,114 +393,42 @@ class AccountController extends AuthController {
         
         $excel = new \PHPExcel();
         
-        
+        //指定工作簿
+        $Excel->setActiveSheetIndex(0); 
         //设置列宽
-        $excel->getActiveSheet()->getColumnDimension('D')->setWidth('14');
-        $excel->getActiveSheet()->getColumnDimension('E')->setWidth('14');
-        $excel->getActiveSheet()->getColumnDimension('F')->setWidth('14');
-        $excel->getActiveSheet()->getColumnDimension('G')->setWidth('14');
-        $excel->getActiveSheet()->getColumnDimension('H')->setWidth('14');
-        
+        $excel->getActiveSheet()->getColumnDimension('A')->setWidth('15');
+        $excel->getActiveSheet()->getColumnDimension('B')->setWidth('15');
+        $excel->getActiveSheet()->getColumnDimension('C')->setWidth('15');
+        $excel->getActiveSheet()->getColumnDimension('D')->setWidth('15');
+        $excel->getActiveSheet()->getColumnDimension('E')->setWidth('15');
+        $excel->getActiveSheet()->getColumnDimension('F')->setWidth('15');
         
         
         $excel->getActiveSheet()->setTitle('交班报表'); //excel标题
         //设置行标题
-        $datahead = array(3 => '微信收入', 4 => '支付宝收入', 5 => '现金收入',6 => '会员卡线下充值',7 => '会员卡支付总额',8 => '退款',9=>'合计');
-        
-     
-        
-        if ($groupbyoperator != '1'  or ($groupbyoperator == '1'  && $groupbydate == '1')){
-            $datahead[0] ='交班时间';
-        }
-        if ($groupbydate != '1'  or ($groupbyoperator == '1'  && $groupbydate == '1')){
-            $datahead[1] ='操作员';
-        }
-        if ($groupbyoperator == '1' || $groupbydate == '1'){
-            unset($datahead[2]);
-        }
-        else
-        {
-            $datahead[2] ='SN码';
-        }
-        ksort($datahead);
-        $i = 0;
-        $sortarr = array();
-        foreach ($datahead as  $v){
-            $sortarr[$i] =$v;
-            $i++;
-        }
-        $datahead =  $sortarr;
-        //动态插入行标题并设置样式
-        foreach ($datahead as $k => $v){
-            $excel->getActiveSheet()->getStyleByColumnAndRow($k,1)
-            ->getFont()->setBold(true);//字体加粗
-            $excel->getActiveSheet()->getStyleByColumnAndRow($k,1)->
-            getAlignment()->setHorizontal(\PHPExcel_Style_Alignment::HORIZONTAL_CENTER);//文字居中
-            $excel->getActiveSheet()->setCellValueByColumnAndRow($k,1,$v);
-        }
-        if($group !=''){
-            $list = M()->fetchSql(false)->query("select dctime,AccountDate,operator,MachineID,sum(WxAmount) WxAmount,sum(ZfbAmount) ZfbAmount,sum(memberpayAmount) memberpayAmount,sum(memberRechargeAmt) memberRechargeAmt,sum(OtherAmount) OtherAmount,sum(RefundAmount) RefundAmount,sum(WxAmount) + sum(ZfbAmount) + sum(memberRechargeAmt) + sum(OtherAmount) as totalmoney from daily_closing where ".$where.$group."   order by dctime desc ");
-        }
-        else
-        {
-            $list = M()->fetchSql(false)->query("select dctime,AccountDate,operator,MachineID,WxAmount,ZfbAmount,memberpayAmount,memberRechargeAmt,OtherAmount,RefundAmount,sum(WxAmount + ZfbAmount + memberRechargeAmt + OtherAmount) as totalmoney from daily_closing where ".$where."  group by dctime order by dctime desc ");
-        }
+        $excel->getActiveSheet()->setCellValue("A1", "姓名");         
+        $excel->getActiveSheet()->setCellValue("B1", "性别");
+        $excel->getActiveSheet()->setCellValue("C1", "移动电话");
+        $excel->getActiveSheet()->setCellValue("D1", "办公电话");
+        $excel->getActiveSheet()->setCellValue("E1", "其他电话");
+        $excel->getActiveSheet()->setCellValue("F1", "备注");
 
-        // $excel->setActiveSheetIndex(0);
-        foreach ($list as $k =>$v){
-            $k = $k+2;
-            if($groupbyoperator == '1'  && $groupbydate == '1'){ //按操作员和时间
-                $excel->setActiveSheetIndex(0)
-                ->setCellValue('A'.$k,$v['accountdate'])
-                ->setCellValue('B'.$k,$v['operator'])
-                ->setCellValue('C'.$k,$v['wxamount']/100 != 0 ? $v['wxamount']/100 : '0')
-                ->setCellValue('D'.$k,$v['zfbamount']/100 != 0 ? $v['zfbamount']/100 : '0')
-                ->setCellValue('E'.$k,$v['otheramount']/100 != 0 ? $v['otheramount']/100 : '0')
-                ->setCellValue('F'.$k,$v['memberrechargeamt']/100 != 0 ? $v['memberrechargeamt']/100 : '0')
-                ->setCellValue('G'.$k,$v['memberpayamount']/100 != 0 ? $v['memberpayamount']/100 : '0')
-                ->setCellValue('H'.$k,$v['refundamount']/100 != 0 ? $v['refundamount']/100 : '0')
-                ->setCellValue('I'.$k,$v['totalmoney']/100 != 0 ? $v['totalmoney']/100 : '0');
-            }   
-            else{
-                if($groupbyoperator == '1'  && $groupbydate != '1'){ //只按操作员
-                    $excel->setActiveSheetIndex(0)
-                    ->setCellValue('A'.$k,$v['operator'])
-                    ->setCellValue('B'.$k,$v['wxamount']/100 != 0 ? $v['wxamount']/100 : '0')
-                    ->setCellValue('C'.$k,$v['zfbamount']/100 != 0 ? $v['zfbamount']/100 : '0')
-                    ->setCellValue('D'.$k,$v['otheramount']/100 != 0 ? $v['otheramount']/100 : '0')
-                    ->setCellValue('E'.$k,$v['memberrechargeamt']/100 != 0 ? $v['memberrechargeamt']/100 : '0')
-                    ->setCellValue('F'.$k,$v['memberpayamount']/100 != 0 ? $v['memberpayamount']/100 : '0')
-                    ->setCellValue('G'.$k,$v['refundamount']/100 != 0 ? $v['refundamount']/100 : '0')
-                    ->setCellValue('H'.$k,$v['totalmoney']/100 != 0 ? $v['totalmoney']/100 : '0');
-                }
-                elseif($groupbyoperator != '1'  && $groupbydate == '1'){ //只按时间
-                    $excel->setActiveSheetIndex(0)
-                    ->setCellValue('A'.$k,$v['accountdate'])
-                    ->setCellValue('B'.$k,$v['wxamount']/100 != 0 ? $v['wxamount']/100 : '0')
-                    ->setCellValue('C'.$k,$v['zfbamount']/100 != 0 ? $v['zfbamount']/100 : '0')
-                    ->setCellValue('D'.$k,$v['otheramount']/100 != 0 ? $v['otheramount']/100 : '0')
-                    ->setCellValue('E'.$k,$v['memberrechargeamt']/100 != 0 ? $v['memberrechargeamt']/100 : '0')
-                    ->setCellValue('F'.$k,$v['memberpayamount']/100 != 0 ? $v['memberpayamount']/100 : '0')
-                    ->setCellValue('G'.$k,$v['refundamount']/100 != 0 ? $v['refundamount']/100 : '0')
-                    ->setCellValue('H'.$k,$v['totalmoney']/100 != 0 ? $v['totalmoney']/100 : '0');
-                }
-                else
-                {
-                    $excel->setActiveSheetIndex(0)
-                    ->setCellValue('A'.$k,$v['dctime'])
-                    ->setCellValue('B'.$k,$v['operator'])
-                    ->setCellValue('C'.$k,$v['machineid'])
-                    ->setCellValue('D'.$k,$v['wxamount']/100 != 0 ? $v['wxamount']/100 : '0')
-                    ->setCellValue('E'.$k,$v['zfbamount']/100 != 0 ? $v['zfbamount']/100 : '0')
-                    ->setCellValue('F'.$k,$v['otheramount']/100 != 0 ? $v['otheramount']/100 : '0')
-                    ->setCellValue('G'.$k,$v['memberrechargeamt']/100 != 0 ? $v['memberrechargeamt']/100 : '0')
-                    ->setCellValue('H'.$k,$v['memberpayamount']/100 != 0 ? $v['memberpayamount']/100 : '0')
-                    ->setCellValue('I'.$k,$v['refundamount']/100 != 0 ? $v['refundamount']/100 : '0')
-                    ->setCellValue('J'.$k,$v['totalmoney']/100 != 0 ? $v['totalmoney']/100 : '0');
-                }
-            }        
-        }
          
+        //获取并填充数据
+        $rs = M('phonebook')->order('contactname')->select();
+        
+        $i=2;
+        foreach ($variable as $key => $value) {
+            $Excel->getActiveSheet()->setCellValue("A".$i,$value["contactname"]);
+            $Excel->getActiveSheet()->setCellValue("B".$i,$value["sex"]);
+            $Excel->getActiveSheet()->setCellValue("C".$i,$value["mobile"]);
+            $Excel->getActiveSheet()->setCellValue("D".$i,$value["officenum"]);
+            $Excel->getActiveSheet()->setCellValue("E".$i,$value["othernum"]);
+            $Excel->getActiveSheet()->setCellValue("F".$i,$value["remark"]);
+            $i++;
+        } 
+        
+      
         $title ='交班报表';
         $date = date('Y-m-d');
         header("Content-Type: application/force-download");
